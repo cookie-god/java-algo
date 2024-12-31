798361cf5c532d65fee44d99319d8d53fd5aee09
cookie-god <lion0193@gmail.com>
2024-12-31 16:33:39 +0900

    feat: 11660번 해결 완료

--- 11660/src/Main.java
+++ 11660/src/Main.java
@@ -10,19 +10,19 @@ public class Main {
     n = sc.nextInt();
     m = sc.nextInt();
 
-    arr = new int[n][n];
-    dp = new int[n][n];
+    arr = new int[n+1][n+1];
+    dp = new int[n+1][n+1];
 
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n; j++) {
         arr[i][j] = sc.nextInt();
       }
     }
 
     int sum;
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
       sum = 0;
-      for (int j = 0; j < n; j++) {
+      for (int j = 1; j <= n; j++) {
         sum += arr[i][j];
         dp[i][j] = sum;
       }
@@ -31,27 +31,23 @@ public class Main {
     int x1, y1, x2, y2;
     int result;
     for (int i = 0; i < m; i++) {
-      x1 = sc.nextInt() - 1;
-      y1 = sc.nextInt() - 1;
-      x2 = sc.nextInt() - 1;
-      y2 = sc.nextInt() - 1;
+      x1 = sc.nextInt();
+      y1 = sc.nextInt();
+      x2 = sc.nextInt();
+      y2 = sc.nextInt();
 
       result = 0;
       for (int j = x1; j <= x2; j++) {
-        if (y1 == 0) {
-          result += (dp[j][y2] - dp[j][0]);
-        } else {
-          result += (dp[j][y2] - dp[j][y1-1]);
-        }
+        result += (dp[j][y2] - dp[j][y1-1]);
       }
       System.out.println(result);
     }
 
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        System.out.print(dp[i][j] + " ");
-      }
-      System.out.println();
-    }
+//    for (int i = 1; i <= n; i++) {
+//      for (int j = 1; j <= n; j++) {
+//        System.out.print(dp[i][j] + " ");
+//      }
+//      System.out.println();
+//    }
   }
 }
