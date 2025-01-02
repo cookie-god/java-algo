b48744b032d344b4a6c8ea197d403f7403b7f5a0
cookie-god <lion0193@gmail.com>
2025-01-02 10:39:07 +0900

    feat: 2841 해결(stack)

--- 2841/src/Main.java
+++ 2841/src/Main.java
@@ -15,12 +15,12 @@ public class Main {
       int line = sc.nextInt();
       int fret = sc.nextInt();
 
-      while (!stacks.get(line).isEmpty() && stacks.get(line).peek() > fret) {
+      while (!stacks.get(line).isEmpty() && stacks.get(line).peek() > fret) { // 비어있거나 스택 최상단이 현재 fret보다 높은 경우 쭉 빼야함
         stacks.get(line).pop();
         answer++;
       }
 
-      if (stacks.get(line).isEmpty() || stacks.get(line).peek() < fret) {
+      if (stacks.get(line).isEmpty() || stacks.get(line).peek() < fret) { // 스택이 비어있거나 현재 스택 최상단이 fret보다 작은 경우만 그러나 위의 조건으로 != 으로 해도 됨
         stacks.get(line).push(fret);
         answer++;
       }
